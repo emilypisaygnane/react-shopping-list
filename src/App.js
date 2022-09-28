3e03a83af09d06cef5c76bd50de2ede9745efc1a
Emily Pisaygnane <episaygnane@me.com>
2022-09-28 15:55:43 -0700

    removed redirect from import list on app.js for build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Auth from './components/Auth/Auth';
 import Header from './components/Header';
