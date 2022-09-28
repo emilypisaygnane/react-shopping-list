bd547fbfc3f5c3ff43ca41cc5d69e253a9bf6e45
Emily Pisaygnane <episaygnane@me.com>
2022-09-28 15:37:55 -0700

    created UserProvided to export user and setUser function

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,1 +1,14 @@
-// TODO -- create the user provider!
+import { useState, createContext } from 'react';
+import { getUser } from '../services/auth';
+
+const UserContext = createContext();
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(currentUser);
+
+  return <UserContext.Provider value={{ user, setUser }}>
+    {children}</UserContext.Provider>;
+
+};
+
+export { UserProvider, UserContext };
